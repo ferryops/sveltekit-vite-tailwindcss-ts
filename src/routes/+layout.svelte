ea52a3d83e4b485093340c7e48b99fc2062b29ab
Ferry Ananda Febian <ferry.a.febian@gmail.com>
2023-08-02 18:06:39 +0800

    sveltekit starter project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../styles.css";
+</script>
+
+<div class="app">
+  <main>
+    <slot />
+  </main>
+
+  <footer class="border-2">
+    <p>
+      visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+    </p>
+  </footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="bg-orange-500">
+  <h1>page</h1>
+</section>
